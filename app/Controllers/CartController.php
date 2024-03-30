e51c3eaa1d55a2e2441bdc2473d615452fc21935
VISHESH SAXENA <visheshsaxena7777@gmail.com>
2024-03-30 18:21:51 +0530

    update

--- app/Controllers/CartController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class CartController {
-    public function index() {
-        include 'app/views/layouts/header.php';
-        include 'app/views/home/index.php';
-        include 'app/views/layouts/footer.php';
-    }
-}
-?>
